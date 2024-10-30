4455e2995a4f3806ca8cbd944a4c39fb2e024bad
Muhammad Qasim <malikqasim20051@gmail.com>
2024-10-30 04:36:31 -0700

    First-commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop:true,
+  
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+      },
+  });
